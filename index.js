343c1367e47cea849978c34f03f6c477aa27ff0f
Hamza Ismail <165427060+HamzaReko@users.noreply.github.com>
2025-05-13 04:23:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const crypto = require('crypto');
 dotenv.config();
 const app = express();
 
